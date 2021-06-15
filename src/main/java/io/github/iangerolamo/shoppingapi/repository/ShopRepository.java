b4e933165d5abd7198eb7108f1b33885745c4e2f
Ian Gerolamo <iangerolamo@gmail.com>
2021-06-14 21:07:12 -0300

    Criando a classe repository para a entidade Shop

---
+++ src/main/java/io/github/iangerolamo/shoppingapi/repository/ShopRepository.java
@@ -0,0 +1,18 @@
+package io.github.iangerolamo.shoppingapi.repository;
+
+import io.github.iangerolamo.shoppingapi.model.Shop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface ShopRepository extends JpaRepository<Shop, Long> {
+
+    public List<Shop> findAllByUserIdentifier(String userIdentifier);
+
+    public List<Shop> findAllByTotalGreaterThan(Float total);
+
+    List<Shop> findAllByDateGreaterThan(Date date);
+}
